e578e8c3af382f45aba456c352dd6e4aff4e1219
Manuel Weiel <manuel@weiel.eu>
2014-05-02 19:31:04 +0200

    removed -Ymacro-debug-lite

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.github.xmanu.virtual-classes",
     version := "0.1",
-    scalacOptions ++= Seq("-deprecation", "-feature", "-Ymacro-debug-lite"),
+    scalacOptions ++= Seq("-deprecation", "-feature"),
     scalaVersion := "2.11.0",
     resolvers += Resolver.sonatypeRepo("snapshots"),
     addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full)
